235160854275a8da2474f72f0251e7dfb74bfdab
MagikarpUsedFly <wr140815@ohio.edu>
2019-02-25 14:46:40 -0500

    added StimulateLife func

--- sequential-ver/GameOfLife.h
+++ sequential-ver/GameOfLife.h
@@ -4,8 +4,31 @@
 // Description:
 //******************************************************************************
 
+#include <iostream>
+#include <vector>
+using namespace std;
+
 class GameOfLife
 {
   public:
+    vector<vector<int> > SimulateLife(vector<vector<int> > &board, int life_cycles);
   private:
+};
+
+
+vector<vector<int> > GameOfLife::SimulateLife(vector<vector<int> > &board, int life_cycles)
+{
+  cout << "hi" << endl;
 }
+
+/*
+// possible neighbors
+1. (i + 1( mod )n, j)
+2. (i − 1) mod n, j)
+3. (i − 1) mod n, j + 1 mod n)
+4. (i − 1) mod n, j − 1 mod n)
+5. (i + 1) mod n, j + 1 mod n)
+6. (i + 1) mod n, j − 1 mod n)
+7. (i, j + 1 mod n)
+8. (i, j − 1 mod n)
+*/
